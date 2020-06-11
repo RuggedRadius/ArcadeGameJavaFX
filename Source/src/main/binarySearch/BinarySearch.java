30235cde1e39da85e504c0925e466326359c6ae0
Ben Royans <ben.royans@gmail.com>
2020-06-11 18:02:12 +0800

    Missed staging some files..

--- Source/src/main/binarySearch/BinarySearch.java
+++ Source/src/main/binarySearch/BinarySearch.java
@@ -1,4 +1,25 @@
 package main.binarySearch;
 
 public class BinarySearch {
+
+    public static int binarySearch(String arr[], int first, int last, String key)
+    {
+        System.out.println("Binary searching");
+
+        if (last>=first)
+        {
+            int mid = first + (last - first)/2;
+            if (arr[mid].equals(key))
+            {
+                return mid;
+            }
+            if (arr[mid].compareTo(key) >= 0) {
+                return binarySearch(arr, first, mid-1, key);//search in left subarray
+            }
+            else {
+                return binarySearch(arr, mid+1, last, key);//search in right subarray
+            }
+        }
+        return -1;
+    }
 }
--- Source/src/main/highScores/FileHandling.java
+++ Source/src/main/highScores/FileHandling.java
@@ -1,18 +1,61 @@
-package main;
+package main.highScores;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.stage.FileChooser;
 
 public class FileHandling
 {
     public static void saveDataFile()
     {
+        try {
+            // Open streams
+            FileOutputStream outputStream = new FileOutputStream(new File("userdata/highscores.dat"));
+            ObjectOutputStream objOutStream = new ObjectOutputStream(outputStream);
 
+            // Write objects to file
+            objOutStream.writeObject(new ArrayList<HighScore>(HighScoreManager.highScores));
+
+            // Close streams
+            objOutStream.close();
+            outputStream.close();
+
+            System.out.println("Highscores saved to file");
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
     }
 
-    public static void loadDataFile(File file)
+    public static void loadDataFile()
     {
+        try {
+            // Open streams
+            FileInputStream inputStream = new FileInputStream(new File("userdata/highscores.dat"));
+            ObjectInputStream objInputStream = new ObjectInputStream(inputStream);
 
+            // Read objects
+            ArrayList<HighScore> scores = (ArrayList<HighScore>) objInputStream.readObject();
+            System.out.println(scores.size() + " highscore records loaded");
+            HighScoreManager.highScores = FXCollections.observableArrayList(scores);
+
+            // Close streams
+            objInputStream.close();
+            inputStream.close();
+
+            System.out.println("Highscores loaded from file");
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.println("No previous highscores found");
+        }
+        catch (Exception ex) {
+            System.err.println("Error loading highscores.dat");
+        }
     }
 
     public static File getLocalFile() {
@@ -20,10 +63,4 @@ public class FileHandling
         FileChooser mrChoosey = new FileChooser();
         return  mrChoosey.showOpenDialog(null);
     }
-
-//    public static File[] getMusicFiles() {
-//
-//
-//        return listOfFiles;
-//    }
 }
